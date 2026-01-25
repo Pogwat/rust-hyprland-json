a00ae6e2fc46b046ef9703d4c9f15561ea63a4a5
pog <208688662+Pogwat@users.noreply.github.com>
2026-01-24 19:26:18 -0500

    add seperate help text

---
+++ src/text.rs
@@ -0,0 +1,15 @@
+pub const HELP: &str = "\
+App
+
+USAGE:
+  app [OPTIONS] 
+
+FLAGS:
+  -h, --help            Prints help information
+
+OPTIONS:
+  -a, --all
+
+ARGS:
+  -p, --path            [path to hyprland IPC socket]
+";
